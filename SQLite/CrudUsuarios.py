01409fde55ccb6d4418f513389bf4530dd448940
OmarDur <125851632+OmarDur@users.noreply.github.com>
2023-03-31 19:39:23 -0600

    Creacion de carpetas y archivos

---
+++ SQLite/CrudUsuarios.py
@@ -0,0 +1,56 @@
+from tkinter import *
+from tkinter import ttk
+import tkinter as tk
+
+Ventana= Tk()
+Ventana.title("CRUD Usuarios")
+Ventana.geometry("500x300")
+
+panel= ttk.Notebook(Ventana)
+panel.pack(fill="both",expand="yes")
+
+pestana1=ttk.Frame(panel)
+pestana2=ttk.Frame(panel)
+pestana3=ttk.Frame(panel)
+pestana4=ttk.Frame(panel)
+
+#Pestana1: Formulario de Usuarios
+titulo= Label(pestana1,text="Registro de Usuarios",fg="Blue",font=("Modern",18) ).pack()
+varNom= tk.StringVar()
+lblNom= Label(pestana1, text="Nombre:").pack()
+txtNom=Entry(pestana1,textvariable=varNom).pack()
+
+varCor= tk.StringVar()
+lblCor= Label(pestana1, text="Correo:").pack()
+txtCor=Entry(pestana1,textvariable=varCor).pack()
+
+varCon= tk.StringVar()
+lblCon= Label(pestana1, text="Contraseña:").pack()
+txtCon=Entry(pestana1,textvariable=varCon).pack()
+
+btnGuardar= Button(pestana1,text="Guardar Usuario").pack()
+
+#Pestaña2:Buscar Usuario
+titulo2= Label(pestana2,text="Buscar usuario",fg="red",font=("Modern",18) ).pack()
+varBus= tk.StringVar()
+lblid= Label(pestana2, text="Identificador de usuario: ").pack()
+txtid=Entry(pestana2,textvariable=varBus).pack()
+btnBusqueda=Button(pestana2,text="Buscar").pack()
+
+subBus=Label(pestana2,text="Registrado: ",fg="blue",font=("Modern",15)).pack()
+TextBus= tk.Text(pestana2,height=5,width=52).pack()
+
+
+
+
+
+
+
+
+panel.add(pestana1,text="Formulario de usuarios")
+panel.add(pestana2,text="Buscar Usuario")
+panel.add(pestana3,text="Consultar Usuarios")
+panel.add(pestana4,text="Actualizar Usuario")
+
+
+Ventana.mainloop()
